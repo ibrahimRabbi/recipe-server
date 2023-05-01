5044d50e43ef5e04c6c10dc001f648dfae8257c8
ibrahim rabbi <ibrahimrabbihere@gmail.com>
2023-05-01 23:23:51 +0600

    setup server for project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 5000;
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.send('server is running')
+})
+
+app.listen(port, ()=>{
+    console.log("server running", port);
+})
